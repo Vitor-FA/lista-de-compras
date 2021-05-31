464c3d51f545b9b61b4099023372a989772d5df0
Vitor F Almeida <vitor.fa2001@gmail.com>
2021-05-31 20:00:33 -0300

    update to main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,71 @@
-const itemInput = document.getElementById("item-input");
-const button = document.getElementById("button");
-const ulItems = document.getElementById("ulitems");
-const liButtons = document.getElementsByClassName("remover");
-const criaElemento = () => {
-    if(itemInput.value === '') return;
-    const li = document.createElement("li");
-    const buttonLi = document.createElement("button")
-    buttonLi.innerHTML = 'X';
-    buttonLi.className = "remover";
-    li.appendChild(buttonLi);
-    li.innerHTML += itemInput.value;
-    ulItems.appendChild(li);
-    itemInput.value = '';
-    liButtonEvents()
+function criaListaDeCompras() {
+    const itemInput = document.getElementById("item-input");
+    const button = document.getElementById("button");
+    const ulItems = document.getElementById("ulitems");
+    const liButtons = document.getElementsByClassName("remover");
+    function criaLi() {return document.createElement("li")};
+    function criaButtonLi() {
+        const geraButtonLi = document.createElement("button")
+        geraButtonLi.innerHTML = 'X';
+        geraButtonLi.className = "remover";
+        return geraButtonLi
+    };
+    function limpaInput() {
+        itemInput.value = '';
+        itemInput.focus();
+    };
+    function criaLista(texto){
+        const li = criaLi();
+        const buttonLi = criaButtonLi();
+        li.appendChild(buttonLi)
+        li.innerHTML += texto;
+        ulItems.appendChild(li);
+        limpaInput();
+        liButtonEvents();
+        salvaLista();
+    };
+
+    function salvaLista() {
+        const liListas = document.querySelectorAll('li');
+        const listaDeCompras = [];
+        for(let lista of liListas){
+            let listaTexto = lista.innerText;
+            listaTexto = listaTexto.replace('X', '').trim();
+            listaDeCompras.push(listaTexto);
+        };
+        const listaJson = JSON.stringify(listaDeCompras);
+        localStorage.setItem('lista', listaJson);
+    };
+
+    function adicionaListaSalva() {
+        const listas = localStorage.getItem('lista');
+        const listaDeCompras = JSON.parse(listas);
+        for(let lista of listaDeCompras) {
+            criaLista(lista);
+        };
+    };
+
+    function deleteItem() {
+        this.parentElement.remove();
+        salvaLista();
+    };
+
+    function liButtonEvents() {
+        for(i = 0; i < liButtons.length; i++){
+            liButtons[i].addEventListener("click", deleteItem);
+        }
+    }
+    itemInput.addEventListener('keypress', function(e) {
+        if(e.key === 'Enter') {
+            criaLista(itemInput.value);
+        };
+    });
+    adicionaListaSalva();
+    liButtonEvents();
+    button.addEventListener("click", ()=>{
+        if(!itemInput.value) return;
+        criaLista(itemInput.value)
+    });
 }
 
-function deleteItem() {
-    this.parentElement.remove();
-};
-function liButtonEvents() {
-    for(i = 0; i < liButtons.length; i++){
-        liButtons[i].addEventListener("click", deleteItem);
-    }
-}
-liButtonEvents()
-button.addEventListener("click", criaElemento);
-const submit = (e) => {if(e.keyCode === 13) criaElemento()};
+criaListaDeCompras();
